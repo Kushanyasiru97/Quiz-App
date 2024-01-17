78ad38a9992e6ed2a112cdb425f24583814817f1
Kushan Yasiru <kushanyasiruwalpola@gmail.com>
2024-01-17 13:00:49 +0530

    React login form complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,79 @@
-import { Box, Button, TextField } from '@mui/material'
-import React from 'react'
+import { Box, Button, Card, CardContent, TextField, Typography} from '@mui/material'
+import React, { useState } from 'react'
+import Center from './Center'
+import useForm from '../hooks/useForm'
 
 export default function Login() {
+
+    const getFreshModel = () =>({
+        name: '',
+        email: ''
+    })
+
+    const {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange
+    } = useForm(getFreshModel);
+
+    const login = e => {
+        e.preventDefault();
+        if(validate())
+            console.log(values);
+    }
+
+    const validate = () => {
+        let temp ={}
+        temp.email = (/\S+@S+\.\S+/).test(values.email) ? "" : "Email is not Valid..."
+        temp.name = values.name!= "" ?"" : "This fiels is Required..."
+        setErrors(temp)
+        return Object.values(temp).every(x=> x == "")
+    }
+
   return (
-    <Box sx={
-        
-    }>
-    <form noValidate autoComplete='off'>
+    <Center>
+    <Card sx={{width:400}}>
+        <CardContent sx={{textAlign: 'center'}}>
+            <Typography variant='h3' sx={{my: 3}}>
+                Quiz App
+            </Typography>
+    <Box sx={{
+        '& .MuiTextField-root': {
+            margin: 1,
+            width: '90%'
+        }
+    }}>
+    <form noValidate autoComplete='off' onSubmit={login}>
         <TextField
         label = "Email"
         name='email'
-        variant='outlined' />
+        value={values.email}
+        onChange={handleInputChange}
+        variant='outlined'
+        {...(errors.email && {error:true, helperText: errors.email})}
+        />
 
 <TextField
         label = "Name"
         name='name'
-        variant='outlined' />
+        value={values.name}
+        onChange={handleInputChange}
+        variant='outlined' 
+        {...(errors.name && {error:true, helperText: errors.name})}
+        />
 
         <Button
         type='submit'
         variant='contained'
-        size='large'>Start</Button>
+        size='large'
+        sx={{width:'90%'}}
+        >Start</Button>
     </form>
     </Box>
+    </CardContent>
+    </Card>
+    </Center>
   )
 }
